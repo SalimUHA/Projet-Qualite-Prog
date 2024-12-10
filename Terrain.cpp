b408713c50408be068f0374e1220cd10968bb969
Sabri Aydi <sabri@MacBook-Air-de-Sabri.local>
2024-12-10 19:04:14 +0100

    Ajout constructeur Terrain.h

---
+++ Terrain.cpp
@@ -0,0 +1,14 @@
+//
+// Created by salim on 10/12/2024.
+
+
+#include "Terrain.h"
+using namespace std;
+
+
+
+Terrain::Terrain(int i): d_i{i}
+{}
+
+
+
---
+++ Terrain.h
@@ -0,0 +1,20 @@
+//
+// Created by salim on 10/12/2024.
+//
+
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+
+
+class Terrain {
+public:
+    Terrain(int i);
+private:
+    int d_i;
+
+};
+
+
+
+#endif //TERRAIN_H
