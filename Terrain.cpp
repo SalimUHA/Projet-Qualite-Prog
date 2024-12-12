6aa037cc66512f48691bef2596173648d66a8d5b
SalimUHA <salimahmida68@gmail.com>
2024-12-12 13:10:46 +0100

    classe position pour le terrain

--- Terrain.cpp
+++ Terrain.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Terrain.h"
+
+Terrain::Terrain(int largeur, int hauteur):
+ d_largeur(largeur), d_hauteur(hauteur)
+{
+  d_grille.resize(hauteur, std::vector<Cellule>(largeur));
+}
+
+int Terrain::obtenirLargeur() const
+    {
+    return d_largeur;
+    }
+int Terrain::obtenirHauteur() const
+        {
+  return d_hauteur;
+  }
--- Terrain.h
+++ Terrain.h
@@ -6,10 +6,30 @@
 #define TERRAIN_H
 #include <iostream>
 #include <vector>
+#include "Position.h"
+#include "Celulle.h"
 
 class Terrain {
 public:
-  Terrain();
+  Terrain(int largeur, int hauteur);
+  int obtenirLargeur() const;
+  int obtenirHauteur() const;
+  Cellule obtenirCelulle(const Position &pos) const;
+  Position obtenirPositionDepart() const;
+  Position obtenirPositionArrivee() const;
+  void definirMur(const Position& pos);
+  void definirDepart(const Position& pos);
+  void definirArrivee(const Position& pos);
+  void chargerDepuisFichier(const std::string &nomFichier);
+  void sauvegarderDansFichier(const std::string &nomFichier) const;
+  void AfficherTerrain() const;
+private:
+  int d_largeur, d_hauteur;
+  std::vector<std::vector<Cellule>> d_grille;
+  Position d_depart;
+  Position d_arrive;
+  Position d_robotPos;
+
 };
 
 
