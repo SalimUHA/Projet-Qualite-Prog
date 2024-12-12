f9f7f95831e0b4fb698e16728db283d9e51fa775
0xsabri <0xsabrii@gmail.com>
2024-12-12 13:16:06 +0100

    "Ajout Terrain.h"

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Sabri Aydi on 10/12/2024.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Votre code ici vfvf
+    std::cout<<"Hello World";
+    return 0;
+}
