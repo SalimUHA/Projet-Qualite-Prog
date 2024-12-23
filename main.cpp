e2e269557438f6d5fe5cd397b41e64e036da9778
Yanis Benamor <benamoryanis06@gmail.com>
2024-12-23 21:56:59 +0100

    ajout de quelques test

--- main.cpp
+++ main.cpp
@@ -3,9 +3,10 @@
 //
 #include <iostream>
 #include "Terrain.h"
-int main()
-   {
+#include "robot.h"
+#include "observateurConsole.h"
 
+void testTerrain(){
     terrain terrain(5, 5);
 
     terrain.definirMur(position(0, 0));
@@ -15,6 +16,44 @@ int main()
 
     terrain.afficherTerrain();
 
-    std::cout<<"test";
+
+}
+
+
+
+int main()
+{
+    terrain terrain(5, 5);
+
+    terrain.definirMur(position(0, 0));
+    terrain.definirMur(position(1, 0));
+    terrain.definirDepart(position(1, 1));
+    terrain.definirArrivee(position(3, 3));
+
+    terrain.afficherTerrain();
+    robot robot(position(0,0));
+
+    observateurConsole observateurConsole;
+    robot.ajouterObservateur(&observateurConsole);
+
+
+    std::cout << "\nDéplacement du robot :" << std::endl;
+
+    robot.avancerUneCase();
+    robot.tournerDroite();
+    robot.avancerUneCase();
+    robot.tournerDroite();
+    robot.avancerUneCase();
+
+
+    if (robot.detecterObstacle(terrain)) {
+        std::cout << "Obstacle détecté ! Le robot ne peut pas avancer." << std::endl;
+    } else {
+        robot.avancerUneCase();
+    }
     return 0;
-  }
+}
+
+
+
+
